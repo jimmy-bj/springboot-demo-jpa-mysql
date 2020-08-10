8cddd4ec30028efd57c82057b3f4edb09b14caea
jimmy-bj <845109641@qq.com>
2020-08-10 19:48:24 +0800

    Initial commit springboot-demo-jpa-mysql

---
+++ src/main/java/com/example/SpringbootDemoJpaMysqlApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDemoJpaMysqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootDemoJpaMysqlApplication.class, args);
+    }
+
+}
